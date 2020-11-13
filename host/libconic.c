45ac82d9f05cbbf0f61259226f166d6dcbaa7e07
Roman Luitko <dossalab@pm.me>
2020-11-14 00:44:55 +0300

    add libconic header

--- host/libconic.c
+++ host/libconic.c
@@ -7,34 +7,24 @@
  * Roman Luitko, 2020
  */
 
-/* TODO: provide a configuration option for that */
-#define ARCH_LITTLE_ENDIAN
-
+#include "libconic.h"
 #include <host/serial.h>
 #include <proto/messages.h>
 #include <proto/packet.h>
 #include <misc/endian.h>
 #include <string.h>
 
-#ifdef _WIN32
-  #define API_EXPORT __declspec(dllexport)
-#else
-  #define API_EXPORT
-#endif
-
-#define CONIC_BAUDRATE	115200
-
 static int enumerate_callback(const char *dev)
 {
 	return serial_open(dev, CONIC_BAUDRATE);
 }
 
-int API_EXPORT conic_open(void)
+int conic_open(void)
 {
 	return serial_enumerate(enumerate_callback);
 }
 
-int API_EXPORT conic_is_open(int fd)
+int conic_is_open(int fd)
 {
 	if (!serial_is_open(fd)) {
 		return -1;
@@ -43,7 +33,7 @@ int API_EXPORT conic_is_open(int fd)
 	return 0;
 }
 
-int API_EXPORT conic_move(int fd, int16_t x, int16_t y, int16_t z)
+int conic_move(int fd, int16_t x, int16_t y, int16_t z)
 {
 	struct serial_packet packet;
 	struct move_packet_payload payload;
@@ -64,12 +54,12 @@ int API_EXPORT conic_move(int fd, int16_t x, int16_t y, int16_t z)
 	return 0;
 }
 
-int API_EXPORT conic_park(int fd, int16_t x, int16_t y, int16_t z)
+int conic_park(int fd, int16_t x, int16_t y, int16_t z)
 {
 	return -1;
 }
 
-void API_EXPORT conic_close(int fd)
+void conic_close(int fd)
 {
 	serial_close(fd);
 }
---
+++ host/libconic.h
@@ -0,0 +1,32 @@
+/*
+ * Part of the Conic project - robot arm controller
+ *
+ * libconic - PC interfacing library
+ *
+ * See LICENSE.md for licensing information
+ * Roman Luitko, 2020
+ */
+
+#ifndef LIBCONIC_H
+#define LIBCONIC_H
+
+#include <stdint.h>
+
+#ifdef _WIN32
+  #define API_EXPORT __declspec(dllexport)
+#else
+  #define API_EXPORT
+#endif
+
+/* TODO: provide a configuration option for that */
+#define ARCH_LITTLE_ENDIAN
+#define CONIC_BAUDRATE	115200
+
+int API_EXPORT conic_open(void);
+int API_EXPORT conic_is_open(int fd);
+int API_EXPORT conic_move(int fd, int16_t x, int16_t y, int16_t z);
+int API_EXPORT conic_park(int fd, int16_t x, int16_t y, int16_t z);
+void API_EXPORT conic_close(int fd);
+
+#endif
+
